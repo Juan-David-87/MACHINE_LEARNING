ca48fee092bbf6dc98978d864d8c8675c4b0316d
yiraparra027-web <yiraparra027@gmail.com>
2026-03-20 20:51:46 -0500

    Machine Learning

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template
+import subprocess
+import sys
+
+from flask import Flask, render_template, request
+import LinearRegression
+import LogisticRegression 
 
 app = Flask(__name__)
 
@@ -9,3 +14,30 @@ def home():
 @app.route('/FirstPage/<name>')
 def firstPage(name):
     return render_template('index.html', name=name)
+
+@app.route("/LinearRegression/", methods=['GET', 'POST'])
+def calculateGrade(): 
+    calculateGrade = None
+    if request.method == 'POST':
+        hours = float(request.form['hours'])
+        calculateGrade = LinearRegression.calculateGrade(hours)
+    return render_template('linearRegressionGrade.html', result=calculateGrade)
+
+@app.route("/LogisticRegression/", methods=['GET', 'POST'])
+def logisticRegression(): 
+    result = None
+
+    if request.method == 'POST':
+        try:
+            proceso = subprocess.run(
+                [sys.executable, "LogisticRegression.py"],
+                capture_output=True,
+                text=True
+            )
+
+            result = proceso.stdout
+
+        except Exception as e:
+            result = f"Error: {str(e)}"
+
+    return render_template('LogisticRegression.html', result=result)
