4de6e1ba705c23ff3c3f27c4f70ae40effd599b8
Juan-David-87 <davidtrolljuan87@gmail.com>
2026-03-23 16:10:10 -0500

    The menu for the four use cases and the home was added

--- app.py
+++ app.py
@@ -4,8 +4,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "hello Flask"
+    return "Machine Learning Use Cases in Flask"
 
-@app.route('/FirstPage/<name>')
-def firstPage(name): 
-    return render_template('index.html',name = name)
+@app.route("/Menu")
+def firstPage(): 
+    return render_template("Menu.html") # este recarga el home toca hacer que este en home y vaya al menu 
+
+@app.route("/FirstCase")
+def secondPage(): 
+    return render_template("UseCaseJuanda.html")
+
+@app.route("/SecondCase")
+def thirdPage(): 
+    return render_template("UseCaseElkin.html")
+
+@app.route("/ThirdCase")
+def fourthPage(): 
+    return render_template("UseCaseYira.html")
+
+@app.route("/FourthCase")
+def fifthPage(): 
+    return render_template("UseCaseJuan.html")
