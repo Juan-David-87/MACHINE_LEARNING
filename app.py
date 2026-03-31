0756b441a93747a9fcbf6b3d14fc093eace68095
yiraparra027-web <yiraparra027@gmail.com>
2026-03-30 22:20:51 -0500

    Application LDA

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from LinearRegressionOil import predictOil, generatePlot as generatePlotLinear
 from LogisticRegressionOil import predictOilCategory, generatePlot as generatePlotLogistic, getThreshold
+from LinearDiscriminantAnalysis import generatePlot, predictOilCategoryLDA, getThreshold
 
 app = Flask(__name__)
 
@@ -56,6 +57,7 @@ def LinearRegression():
         plot = generatePlotLinear()
 
     return render_template("OilLinearRegression.html", result=result, plot=plot)
+
 @app.route("/OilLogisticRegression", methods=["GET", "POST"])
 def oil_logistic():
     result = None
@@ -78,3 +80,26 @@ def oil_logistic():
     year_value=year_value,
     threshold=threshold
 )
+
+
+@app.route("/LinearDiscriminantAnalysis", methods=["GET", "POST"])
+def oil_lda():
+    result = None
+    plot = None
+    year_value = None
+    probability = None
+    threshold = getThreshold()
+
+    if request.method == "POST":
+        year_value = int(request.form["year"])
+        result, probability = predictOilCategoryLDA(year_value)
+        plot = generatePlot(year_value)
+
+    return render_template(
+        "LinearDiscriminantAnalysis.html",
+        result=result,
+        probability=probability,
+        plot=plot,
+        year_value=year_value,
+        threshold=threshold
+    )
